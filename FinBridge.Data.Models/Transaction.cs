d6557c665f84b383550294e20dca9452375eda0c
Ivan Ivanov <ivanov.github@gmail.com>
2025-04-06 00:11:28 +0300

    Finished working on Transaction class

--- FinBridge.Data.Models/Transaction.cs
+++ FinBridge.Data.Models/Transaction.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FinBridge.Data.Models.TransactionEnums;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FinBridge.Data.Models
 {
@@ -10,6 +13,26 @@ namespace FinBridge.Data.Models
         [Required]
         public decimal Amount { get; set; }
 
+        [Required]
+        public TransactionType TransactionType { get; set; }
 
+        [Required]
+        public DateTime Date { get; set; }
+
+        [ForeignKey(nameof(SenderAccount))]
+        public int? SenderAccountId { get; set; }
+        public BankAccount? SenderAccount { get; set; }
+
+        [ForeignKey(nameof(ReceiverAccount))]
+        public int? ReceiverAccountId { get; set; }
+        public BankAccount? ReceiverAccount { get; set; }
+
+        [ForeignKey(nameof(Customer))]
+        public int? CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+
+        [Unicode(true)]
+        [MaxLength(500)]
+        public string? Note { get; set; }
     }
 }

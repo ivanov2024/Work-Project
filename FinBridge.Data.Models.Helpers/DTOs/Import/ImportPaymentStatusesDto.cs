e511623c740509485eec8e052f76658e48578fdf
Ivan Ivanov <ivanov.github@gmail.com>
2025-04-08 12:21:02 +0300

    Added ImportPaymentStatusesDto class for importing data from paymentStatuses.json file

---
+++ FinBridge.Data.Models.Helpers/DTOs/Import/ImportPaymentStatusesDto.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinBridge.Data.Models.Helpers.DTOs.Import
+{
+    public class ImportPaymentStatusesDto
+    {
+        [Required]
+        [JsonProperty(nameof(Status))]
+        public string Status { get; set; }
+            = null!;
+
+        [Required]
+        [JsonProperty(nameof(Code))]
+        public string Code { get; set; }
+            = null!;
+    }
+}

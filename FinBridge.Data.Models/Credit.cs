02b26098e9f334c7493e7f864c135ef859fb69be
Ivan Ivanov <ivanov.github@gmail.com>
2025-04-09 21:12:56 +0300

    Added CreditType enum and Credit class

---
+++ FinBridge.Data.Models/Credit.cs
@@ -0,0 +1,45 @@
+﻿using FinBridge.Data.Models.Enums.CreditEnums;
+using FinBridge.Data.Models.Enums.PaymentEnums;
+using FinBridge.Data.Models.TransactionEnums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinBridge.Data.Models
+{
+    public class Credit
+    {
+        [Key]
+        public int CreditId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Customer))]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public Customer Customer { get; set; }
+            = null!;
+
+        [Required]
+        public CreditType CreditType { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public decimal InterestRate { get; set; }
+
+        [Required]
+        public int TermMonths { get; set; }
+
+        [Required]
+        public DateTime ApplicationDate { get; private set; } 
+            = DateTime.Now;
+
+        public DateTime? ApprovalDate { get; set; }
+
+        public DateTime? DisbursementDate { get; set; }
+
+        [Required]
+        public PaymentStatus Status { get; set; }
+    }
+}
---
+++ FinBridge.Data.Models/Enums/CreditEnums/CreditType.cs
@@ -0,0 +1,22 @@
+﻿namespace FinBridge.Data.Models.Enums.CreditEnums
+{
+    public enum CreditType
+    {
+        Personal = 1,
+        Mortgage = 2,
+        Auto = 3,
+        Student = 4,
+        Business = 5,
+        Payday = 6,
+        HomeImprovement = 7,
+        CreditCard = 8,
+        Consolidation = 9,
+        Agricultural = 10,
+        Travel = 11,
+        Wedding = 12,
+        Medical = 13,
+        Green = 14,
+        Emergency = 15
+    }
+
+}

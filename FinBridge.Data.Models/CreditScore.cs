e5b89160d085842d20b6b437cb8be799ade4741e
Ivan Ivanov <ivanov.github@gmail.com>
2025-04-09 21:35:50 +0300

    Added CreditScore class for saving customers' credit score

---
+++ FinBridge.Data.Models/CreditScore.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinBridge.Data.Models
+{
+    public class CreditScore
+    {
+        [Key]
+        public int CreditScoreId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Customer))]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public Customer Customer { get; set; } = null!;
+
+        [Required]
+        [Range(0, 1000)]
+        public int Score { get; set; }
+    }
+}

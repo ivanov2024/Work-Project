a3697c24584a9872a2bb80d26dc3fc7cbf3d6488
Ivan Ivanov <ivanov.github@gmail.com>
2025-04-10 10:33:25 +0300

    Added C# library FinBridge.Data for configuring the Fluent API. The FinBridgeConnection class holds the connection string required to connect the application to the database.

---
+++ FinBridge.Data/FinBridgeConnection .cs
@@ -0,0 +1,8 @@
+﻿namespace FinBridge.Data
+{
+    internal class FinBridgeConnection
+    {
+        internal static readonly string ConnectionString
+            = "Server =.; Database=FinBridge;Integrated Security = True; TrustServerCertificate=True";
+    }
+}

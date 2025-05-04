8c13c09d50d5aa4c3f3f9c27e9aa45d7ab1120e9
Ivan Ivanov <ivanov.github@gmail.com>
2025-05-04 21:24:54 +0300

    Made some UI changes

---
+++ FinBridge.App/Components/AppIcon.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace FinBridge.App.Components
+{
+    public partial class AppIcon : ContentView
+    {
+        public AppIcon()
+        {
+            InitializeComponent();
+        }
+    }
+} 
